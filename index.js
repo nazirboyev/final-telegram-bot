8c91e5f6d7e96f86267190dfc322c67bc2f1ce20
xudoyshukur <xudoyshukurnazirboyev@gmail.com>
2025-12-04 17:07:00 +0500

    done

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+import mongoose from "mongoose";
 import "./src/bot/bot.js";
+
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() =>{
+    console.log(`DB is connected...`);
+})
+.catch(() => {
+    console.log(`Error: db is not connected!!!`);
+    
+})
+
 
 console.log("Dastur boshlanmoqda...");
--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,1 +1,18 @@
+import TelegramBot from "node-telegram-bot-api";
+import { config } from "dotenv";
+config();
+
+const bot = new TelegramBot(process.env.BOT_TOKEN, {polling:true});
+
+bot.on("message", (msg) => {
+    const chatId = msg.chat.id;
+    const firstname = msg.chat.first_name;
+    const text = msg.text;
+    
+    
+    bot.sendMessage(chatId, `Assalomu Alaykum ${firstname1}`)
+    bot.sendMessage(chatId, `text: ${text}`)
+
+})
+
 console.log("Bot ishga tushdi...");
